617c703f938086eb1bcb8a94d2478036f9056a9e
John <77780772+JMcrafter26@users.noreply.github.com>
2023-01-05 19:08:37 +0100

    New version 🥳🥳

--- statistics-endpoint/statistics.php
+++ statistics-endpoint/statistics.php
@@ -10,14 +10,29 @@
 //check Hash
 $hash = md5('statistics' . date('h-i-d-m-Y'));
 if (isset($_GET['exp']) && $_GET['exp'] == $hash) {
-    echo 'success';
     //get data
     $data = file_get_contents('statistics.json');
     $data = json_decode($data, true);
     $data['count'] = $data['count'] + 1;
     $data = json_encode($data);
     file_put_contents('statistics.json', $data);
-    die();
+    //check for new stable version of PHP Badges (https://test.jm26.net/shields-badges) and compare to $_GET['v']
+      if(isset($_GET['v'])) {
+
+        $stable = file_get_contents('https://raw.githubusercontent.com/JMcrafter26/php-badges/main/stable-version.json');
+        $stable = json_decode($stable, true);
+        $stable = $stable['Version']['Stable'];
+        // remove dots from version
+        $stable = str_replace('.', '', $stable);
+        $current = $_GET['v'];
+        $current = str_replace('.', '', $current);
+        if($stable > $current) {
+          echo 'update';
+        }else{
+          echo 'success';
+        }
+}
+die();
 }
 
 //display badge from https://test.jm26.net/api/badge?label=Times%20Generated&message=0&color=blue
@@ -27,7 +42,17 @@ $data = file_get_contents('statistics.json');
 $data = json_decode($data, true);
 $count = $data['count'];
 
+if($_GET['accuratecount'] != 'true') {
+if ($count >= 1000000000) {
+  $count = round($count / 1000000000, 1) . 'B';
+} elseif ($count >= 1000000) {
+  $count = round($count / 1000000, 1) . 'M';
+} elseif ($count >= 1000) {
+  $count = round($count / 1000, 1) . 'K';
+}
+}
+
+
 //display badge
 header('Content-Type: image/png');
 readfile('https://test.jm26.net/api/badge?label=Times%20Generated&message=' . $count . '&color=blue');
-?>
