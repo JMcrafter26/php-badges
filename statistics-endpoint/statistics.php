2b8126a94165439d318d3705f477b1c882efba3b
John <77780772+JMcrafter26@users.noreply.github.com>
2023-01-07 16:01:08 +0100

    Add files via upload

--- statistics-endpoint/statistics.php
+++ statistics-endpoint/statistics.php
@@ -8,39 +8,40 @@
  */
 
 //check Hash
-$hash = md5('statistics' . date('h-i-d-m-Y'));
-if (isset($_GET['exp']) && $_GET['exp'] == $hash) {
-    //get data
-    $data = file_get_contents('statistics.json');
-    $data = json_decode($data, true);
-    $data['count'] = $data['count'] + 1;
-    $data = json_encode($data);
-    file_put_contents('statistics.json', $data);
-    //check for new stable version of PHP Badges (https://test.jm26.net/shields-badges) and compare to $_GET['v']
-      if(isset($_GET['v'])) {
+if (isset($_GET['exp'])) {
+  $hash = md5('statistics' . date('h-i-d-m-Y'));
+  if($_GET['exp'] != $hash) {
+    header('HTTP/1.1 403 Forbidden');
+    exit();
+  }
+  if(isset($_GET['v'])) {
+    $stable = file_get_contents('https://raw.githubusercontent.com/JMcrafter26/php-badges/main/version.json');
+    $stable = json_decode($stable, true);
+    $stable = $stable['Version']['Stable'];
+    // remove dots from version
+    $stable = str_replace('.', '', $stable);
+    $current = $_GET['v'];
+    $current = str_replace('.', '', $current);
+    header('HTTP/1.1 200 OK');
+    if($stable > $current) {
+      echo 'update';
+    }else{
+      echo 'success';
+    }
+  }
 
-        $stable = file_get_contents('https://raw.githubusercontent.com/JMcrafter26/php-badges/main/stable-version.json');
-        $stable = json_decode($stable, true);
-        $stable = $stable['Version']['Stable'];
-        // remove dots from version
-        $stable = str_replace('.', '', $stable);
-        $current = $_GET['v'];
-        $current = str_replace('.', '', $current);
-        if($stable > $current) {
-          echo 'update';
-        }else{
-          echo 'success';
-        }
-}
-die();
-}
+    //get data
+    $data = file_get_contents('count.txt');
+    // add 1 to count
+    $data = $data + 1;
+    file_put_contents('count.txt', $data);
+    exit();
+} else {
 
 //display badge from https://test.jm26.net/api/badge?label=Times%20Generated&message=0&color=blue
 
 //get data
-$data = file_get_contents('statistics.json');
-$data = json_decode($data, true);
-$count = $data['count'];
+$count = file_get_contents('count.txt');
 
 if($_GET['accuratecount'] != 'true') {
 if ($count >= 1000000000) {
@@ -54,5 +55,9 @@ if ($count >= 1000000000) {
 
 
 //display badge
+header('HTTP/1.1 200 OK');
 header('Content-Type: image/png');
-readfile('https://test.jm26.net/api/badge?label=Times%20Generated&message=' . $count . '&color=blue');
+readfile('https://test.jm26.net/api/badge?label=Times%20Generated&message=' . $count . '&color=blue&format=png');
+
+exit();
+}
